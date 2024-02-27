946daa0fa226b54c02b910937c73227ebe8eb474
pypatra <pikiyolputra@gmail.com>
2024-02-27 22:05:08 +0700

    maadasda

--- day-11.7.py
+++ day-11.7.py
@@ -1,5 +1,8 @@
+#cara 1
 def validate_pin(pin: str) -> bool:
     return [False, True][(len(pin) == 4 or len(pin) == 6) and (pin.isdigit())]
 
-
+# cara 2
+def validate_pin(pin: str) -> bool:
+  return len(pin) in [4,6] and pin.isdigit()
 print(validate_pin("090988"))
